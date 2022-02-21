0585c89c4efff11720af845c4e78c20fd01a6757
Werberty Alexandre <werberty.alexandre@gmail.com>
2022-02-21 11:12:52 -0300

    Dicionando metodo de formatar a str e o de converter para binario

--- main.py
+++ main.py
@@ -1,2 +1,40 @@
+from posixpath import split
+import re
+
 class CalculaIpv4:
-    pass
+    def __init__(self, ip, mascara=None) -> None:
+        self.ip = ip
+        self.mascara = mascara
+
+    def teste(self):
+        ip, bit_de_rede = self.remover_caracteres(self.ip)
+        print(f'IP: {ip}, Bit rede: {bit_de_rede}')
+        self.decimal_para_binario(ip)
+
+    def decimal_para_binario(self, ip):
+        ip_binario = []
+        for decimal in ip:
+            dec_binario = ''
+            while True:
+                if decimal % 2:
+                    dec_binario += '1'
+                else:
+                    dec_binario += '0'
+                decimal = decimal // 2
+                if decimal <= 0:
+                    break
+            if len(dec_binario) < 8:
+                zeros = 8 - len(dec_binario)
+                dec_binario += '0'*zeros
+            dec_binario = dec_binario[::-1]
+            ip_binario.append(dec_binario)
+        print(ip_binario)
+
+    def remover_caracteres(self, ip):
+        ip, bit_rede = ip.split('/')
+        ip = [int(n) for n in ip.split('.')]
+        return ip, int(bit_rede)
+
+ip = '10.20.12.45/26'
+calc_ip = CalculaIpv4(ip)
+calc_ip.teste()
