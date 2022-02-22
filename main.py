0b96c89e4334dce9a7af7c4454014c8c08f0fd36
Werberty Alexandre <werberty.alexandre@gmail.com>
2022-02-22 08:16:27 -0300

    Adicionando método de converter ip binário em decimal

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from posixpath import split
 import re
 
 class CalculaIpv4:
@@ -9,26 +8,27 @@ class CalculaIpv4:
     def teste(self):
         ip, bit_de_rede = self.remover_caracteres(self.ip)
         print(f'IP: {ip}, Bit rede: {bit_de_rede}')
-        self.decimal_para_binario(ip)
+        print(self.decimal_para_binario(ip))
+        print(self.binario_para_decimal(ip))
+
+    def binario_para_decimal(self, ip):
+        ip_decimal = []
+        for binario in ip:
+            binario_dec = format(binario, 'd')
+            ip_decimal.append(int(binario_dec))
+        return ip_decimal
 
     def decimal_para_binario(self, ip):
         ip_binario = []
         for decimal in ip:
-            dec_binario = ''
-            while True:
-                if decimal % 2:
-                    dec_binario += '1'
-                else:
-                    dec_binario += '0'
-                decimal = decimal // 2
-                if decimal <= 0:
-                    break
+            dec_binario = format(decimal, 'b')
+            dec_binario = dec_binario[::-1]
             if len(dec_binario) < 8:
                 zeros = 8 - len(dec_binario)
                 dec_binario += '0'*zeros
             dec_binario = dec_binario[::-1]
             ip_binario.append(dec_binario)
-        print(ip_binario)
+        return ip_binario
 
     def remover_caracteres(self, ip):
         ip, bit_rede = ip.split('/')
@@ -37,4 +37,8 @@ class CalculaIpv4:
 
 ip = '10.20.12.45/26'
 calc_ip = CalculaIpv4(ip)
+a = format(10, 'b')
+b = []
+b.append(a)
+print(b)
 calc_ip.teste()
