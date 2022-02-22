be76a1f50a7c33d1d99c6ec19919d882fdc55608
Werberty Alexandre <werberty.alexandre@gmail.com>
2022-02-22 09:48:48 -0300

    Adicionando método de calcular mascara da rede

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 import re
+from unicodedata import decimal
 
 class CalculaIpv4:
-    def __init__(self, ip, mascara=None) -> None:
+    def __init__(self, ip) -> None:
         self.ip = ip
-        self.mascara = mascara
+        self.nucle_ip, self.bit_de_rede = self.remover_caracteres(ip)
+        self.mascara = self.mascara_sub_rede(self.nucle_ip, self.bit_de_rede)
 
     def teste(self):
         ip, bit_de_rede = self.remover_caracteres(self.ip)
         print(f'IP: {ip}, Bit rede: {bit_de_rede}')
         print(self.decimal_para_binario(ip))
-        print(self.binario_para_decimal(ip))
+        print(self.mascara_sub_rede(ip, bit_de_rede))
+    
+    def mascara_sub_rede(self, ip, bit_de_rede):
+        ip_binario = self.decimal_para_binario(ip)
+        mascara = []
+        cont = 0
+        for binario in ip_binario:
+            valor_mascara = ''
+            for n in binario:
+                if cont < bit_de_rede:
+                    valor_mascara += '1'
+                else:
+                    valor_mascara += '0'
+                cont += 1
+            mascara.append(valor_mascara)
+        mascara = self.binario_para_decimal(mascara)
+        return mascara
 
     def binario_para_decimal(self, ip):
         ip_decimal = []
         for binario in ip:
-            binario_dec = format(binario, 'd')
-            ip_decimal.append(int(binario_dec))
+            binario_dec = int(binario, 2)
+            ip_decimal.append(binario_dec)
         return ip_decimal
 
     def decimal_para_binario(self, ip):
@@ -37,8 +55,6 @@ class CalculaIpv4:
 
 ip = '10.20.12.45/26'
 calc_ip = CalculaIpv4(ip)
-a = format(10, 'b')
-b = []
-b.append(a)
-print(b)
-calc_ip.teste()
+print(calc_ip.bit_de_rede)
+print(calc_ip.mascara)
+# calc_ip.teste()
