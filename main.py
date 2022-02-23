bc6edce411560cabe184371038a07f4692027657
Werberty Alexandre <werberty.alexandre@gmail.com>
2022-02-23 14:28:38 -0300

    Adicionando método de calcular o ip para a rede

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+import enum
 import re
+
 
 class CalculaIpv4:
     def __init__(self, ip) -> None:
         self.ip = ip
         self.corpo_ip, self.bit_de_rede = self.remover_caracteres(ip)
-        self.mascara = self.calc_mascara_sub_rede(self.corpo_ip, self.bit_de_rede)
+        self.mascara = self.calc_mascara_sub_rede(
+            self.corpo_ip, self.bit_de_rede)
         self.numero_ips = self.calc_numero_de_ips(self.mascara)
+        self.rede = self.calc_ip_rede(self.corpo_ip, self.mascara)
+        self.broadcast = None
+        self.primeiro_ip = None
+        self.ultimo_ip = None
 
     def teste(self):
         ip, bit_de_rede = self.remover_caracteres(self.ip)
@@ -14,7 +21,20 @@ class CalculaIpv4:
         print(self.decimal_para_binario(ip))
         print(self.calc_mascara_sub_rede(ip, bit_de_rede))
         print(self.formatar_ip(ip))
-    
+
+    def calc_ip_rede(self, ip, mascara):  # terminando
+        mascara, bit_mascara = self.remover_caracteres(mascara)
+        mascara = self.decimal_para_binario(mascara)
+        ip = self.decimal_para_binario(ip)
+        bit_ip = list(ip[3])
+        for k, valor in enumerate(mascara[3]):
+            if valor == '0':
+                bit_ip[k] = '0'
+        ip[3] = ''.join(bit_ip)
+        ip = self.binario_para_decimal(ip)
+        ip = self.formatar_ip(ip, bit_mascara)
+        return ip
+
     def calc_numero_de_ips(self, mascara):
         mascara, bit_rede = self.remover_caracteres(mascara)
         bits_da_mascara = self.decimal_para_binario(mascara)[3]
@@ -23,7 +43,7 @@ class CalculaIpv4:
             if n == '0':
                 b += 1
         return 2**b - 2
-    
+
     def calc_mascara_sub_rede(self, ip, bit_rede):
         ip_binario = self.decimal_para_binario(ip)
         mascara = []
@@ -37,7 +57,8 @@ class CalculaIpv4:
                     valor_mascara += '0'
                 cont += 1
             mascara.append(valor_mascara)
-        mascara = self.formatar_ip(self.binario_para_decimal(mascara), bit_rede)
+        mascara = self.formatar_ip(
+            self.binario_para_decimal(mascara), bit_rede)
         return mascara
 
     @staticmethod
@@ -60,7 +81,7 @@ class CalculaIpv4:
             dec_binario = dec_binario[::-1]
             ip_binario.append(dec_binario)
         return ip_binario
-    
+
     @staticmethod
     def formatar_ip(ip, bit_de_rede=None):
         novo_ip = '.'.join(map(str, ip))
@@ -74,9 +95,11 @@ class CalculaIpv4:
         ip = [int(n) for n in ip.split('.')]
         return ip, int(bit_rede)
 
+
 ip = '10.20.12.45/26'
 calc_ip = CalculaIpv4(ip)
-print(calc_ip.bit_de_rede)
-print(calc_ip.mascara)
-print(calc_ip.numero_ips)
+print(f'IP: {calc_ip.ip}')
+print(f'Rede: {calc_ip.rede}')
+print(f'Máscara: {calc_ip.mascara}')
+print(f'N° de ips: {calc_ip.numero_ips}')
 # calc_ip.teste()
